a47ca41e6dc397face1ba665c2db258c77ff15b4
MahmoudelBaradie <610@awtad.sa>
2026-02-26 13:10:18 +0300

    Harden security XML to remove category refs and bump version

--- real_estate_security/__manifest__.py
+++ real_estate_security/__manifest__.py
@@ -1,1 +1,7 @@
-{"name":"Real Estate Security Roles","version":"18.0.1.0.0","depends":["real_estate_reports"],"data":["security/security.xml"],"installable":True}
+{
+    'name': 'Real Estate Security Roles',
+    'version': '18.0.1.0.1',
+    'depends': ['real_estate_reports'],
+    'data': ['security/security.xml'],
+    'installable': True
+}
